56f97b54a64d9ca634e1b89fc911e31f1b0693f3
Hugo Ernesto <mail.hbler@gmail.com>
2022-06-16 19:00:19 -0300

    feat! : task done

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,8 @@
 import { useHistory } from "react-router-dom";
+
 import Button from "../../components/buttons/buttons";
 import { Logo } from "../../components/logo/style";
+import { SlideDown, SlideUp } from "../../components/frames";
 import { HomeMain } from "./style";
 
 export default function Home({ authenticated }) {
@@ -12,32 +14,38 @@ export default function Home({ authenticated }) {
         <Logo>KenzieHub</Logo>
 
         {authenticated ? (
-          <Button
-            btnColor="negative"
-            onClick={() => {
-              history.push("/user");
-            }}
-          >
-            Perfil
-          </Button>
+          <SlideDown>
+            <Button
+              btnColor="negative"
+              onClick={() => {
+                history.push("/user");
+              }}
+            >
+              Perfil
+            </Button>
+          </SlideDown>
         ) : (
+          <SlideDown>
+            <Button
+              btnColor="light-gray"
+              onClick={() => {
+                history.push("/login");
+              }}
+            >
+              Fazer Login
+            </Button>
+          </SlideDown>
+        )}
+        <SlideUp>
           <Button
-            btnColor="light-gray"
+            btnColor="gray"
             onClick={() => {
-              history.push("/login");
+              history.push("/registry");
             }}
           >
-            Fazer Login
+            Ver Usuários Cadastrados
           </Button>
-        )}
-        <Button
-          btnColor="gray"
-          onClick={() => {
-            history.push("/registry");
-          }}
-        >
-          Ver Usuários Cadastrados
-        </Button>
+        </SlideUp>
       </HomeMain>
     </div>
   );
